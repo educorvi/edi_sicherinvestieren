5170c94a596f0ec3531de0bbf5d24f3d5aa6fd8e
Julian <neferin42@gmail.com>
2019-08-23 09:17:36 +0200

    Performanceverbesserungen - Lazy Loading im Router - Bereichsweises importieren von Vue Material

--- src/main.js
+++ src/main.js
@@ -7,14 +7,17 @@ import BootstrapVue from "bootstrap-vue";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import VueMaterial from "vue-material";
+
+import {MdBottomBar, MdButton, MdIcon} from "vue-material/dist/components"
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-Vue.use(VueMaterial);
+Vue.use(MdIcon);
+Vue.use(MdButton);
+Vue.use(MdBottomBar);
 new Vue({
     router,
     store,
--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Fragebogen from "./views/Fragebogen"
 
 Vue.use(Router);
 
@@ -10,20 +8,17 @@ export default new Router({
         {
             path: '/',
             name: 'home',
-            component: Home
+            component: () => import('./views/Home.vue')
         },
         {
             path: '/liste/:offen',
             name: 'listen',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/Listen.vue')
+            component: () => import('./views/Listen.vue')
         },
         {
             path: '/:fragebogen/:frage',
             name: 'fragebogen',
-            component: Fragebogen
+            component: () => import('./views/Fragebogen.vue')
         }
     ]
 })
