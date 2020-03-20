e849ebff233b6463ea41be4aa2ada052dd6d840c
Neferin <neferin42@gmail.com>
2020-03-20 15:00:08 +0100

    mobile Optimierung

--- src/views/Einstellungen.vue
+++ src/views/Einstellungen.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
         <div v-if="loggedIn">
-            <b-row align-v="center" i>
-                <b-col cols="4">
+            <b-row align-v="center" align-h="center">
+                <b-col cols="12" md="auto">
                     <b-button @click="logout">Verbindung aufheben</b-button>
                 </b-col>
-                <b-col cols="8">
-                    <p class="text-muted m-0">Hierdurch wird die Verbindung bzw Synchronisierung mit der Cloud
+                <b-col col lg="8">
+                    <p class="text-muted m-0 mt-1">Hierdurch wird die Verbindung bzw Synchronisierung mit der Cloud
                         aufgehoben, es
                         werden jedoch weder lokal, noch in der Cloud Daten gelöscht</p>
                 </b-col>
             </b-row>
             <hr>
         </div>
-        <b-row align-v="center">
-            <b-col cols="4">
+        <b-row align-v="center" align-h="center">
+            <b-col cols="12" md="auto">
                 <b-button variant="danger" @click="deleteData">Daten löschen</b-button>
             </b-col>
-            <b-col cols="8">
-                <p class="text-muted m-0">Alle Daten werden gelöscht</p>
+            <b-col col lg="8">
+                <p class="text-muted m-0 mt-1">Alle Daten werden gelöscht</p>
             </b-col>
         </b-row>
 
