078cb521165c0f2c0716667f5cc15cd7a52bb83e
Julian Pollinger <neferin42@gmail.com>
2020-08-19 16:03:37 +0200

    Teilen per Link

--- src/views/Auswertung.vue
+++ src/views/Auswertung.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center">
     <div v-if="fragebogen">
-      <b-button @click="share" variant="primary" class="w-100">Teilen</b-button>
+      <b-button @click="shareLink" variant="primary" class="w-100">Teilen</b-button>
       <hr>
       <Auswertungsfrage :frage="fragebogen.items[i]" :key="i+fragebogen.items[i].toString()"
                         :selected="item.selected[i]"
@@ -20,6 +20,7 @@ import CustomSpinner from "../components/Helper/CustomSpinner";
 import Auswertungsfrage from "../components/Helper/Auswertungsfrage";
 import Hinweis from "../components/Hinweis";
 import {getAllListen, getListe} from "../js/localDatabase";
+// eslint-disable-next-line no-unused-vars
 import LZString from "../libs/lz-string";
 import config from "../config.json";
 
@@ -30,41 +31,61 @@ export default {
     return {
       fragebogen: null,
       item: null,
-      shareLink: ""
+      link: ""
     }
   },
 
-  mounted() {
+  created() {
     getAllListen().then(() => {
-      this.item = getListe(this.$route.query.name)[0];
-      //Abrufen des Fragebogens
-      this.http.get(this.item.fragebogen + "?fullobjects=true").then(res => {
-        this.fragebogen = res.data;
-      }).catch(err => {
-        console.error(err);
+      if (this.$route.query.shared) {
+        this.item = JSON.parse(LZString.decompress(JSON.parse(this.$route.query.data)));
+      } else {
+        this.item = getListe(this.$route.query.name)[0];
+      }
+      if (this.item) {
+        //Abrufen des Fragebogens
+        this.http.get(this.item.fragebogen + "?fullobjects=true").then(res => {
+          this.fragebogen = res.data;
+        }).catch(err => {
+          console.error(err);
+          this.$root.$bvToast.toast("Laden der Auswertung fehlgeschlagen", {
+            title: "Fehler",
+            variant: "danger",
+            autoHideDelay: 5000
+          })
+          this.$router.push("/?subito=true");
+        });
+      } else {
         this.$root.$bvToast.toast("Laden der Auswertung fehlgeschlagen", {
           title: "Fehler",
           variant: "danger",
           autoHideDelay: 5000
         })
         this.$router.push("/?subito=true");
-      });
+      }
+
     })
   },
   methods: {
-    share() {
-      this.shareLink = config.baseURL + "auswertung?shared=true&data=" + LZString.compress(JSON.stringify(this.item));
+    shareLink() {
+      this.link = config.baseURL + "auswertung?shared=true&data=" + encodeURIComponent(JSON.stringify(LZString.compress((JSON.stringify(this.item)))));
+      console.log(this.link.length)
       const shareData = {
         title: "Auswertung von \"" + this.item.name + "\"",
         text: "Auswertung von \"" + this.item.name + "\" bezüglich des Fragebogens \"" + this.item.fragebogenName + "\"",
-        url: this.shareLink
+        url: this.link
       }
       if (navigator.canShare && navigator.canShare(shareData)) {
         navigator.share(shareData)
             .then(() => console.log('Share was successful.'))
             .catch((error) => console.log('Sharing failed', error));
       } else {
-        console.log(`Your system doesn't support sharing files.`);
+        navigator.clipboard.writeText(this.link);
+        this.$bvToast.toast("Link zum Teilen in das Clipboard kopiert. Sie können ihn nun an einem beliebigen Ort einfügen", {
+          title: "Kopiert",
+          // variant: "danger",
+          autoHideDelay: 10000
+        })
       }
     }
   },
