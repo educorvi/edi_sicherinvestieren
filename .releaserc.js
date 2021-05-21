bdc4ec6e126b486a1be04045172025216fd9defb
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-05-21 11:24:24 +0200

    Chore: fix release types
    Fixes release types in .commitlintrc.json and .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -10,7 +10,7 @@ module.exports = {
         }],
         ["@semantic-release/git", {
             "assets": ["package.json", "sw.js"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            "message": "Chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }]
     ],
     "preset": "eslint"
